01b78bf5ee746c10cfe8b2cd451e24f4a5bca493
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-07-06 19:12:40 +0100

    Extract letters from source sentence

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ class CommonLettersException(Exception):
 
 
 def three_most_common_letters(sentence):
+  characters = list(sentence.lower())
+  letters = []
+  for character in characters:
+    if character >= 'a' and character <= 'z':
+      letters.append(character)
   pass
   
 
 if __name__ == '__main__':
-  common_letters = three_most_common_letters('common letters in a sentence')
+  common_letters = three_most_common_letters('Write a function that takes in a sentence and returns the 3 most common letters.')
   for letter in common_letters:
     print(letter)
