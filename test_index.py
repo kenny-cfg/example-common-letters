f468d4b2fd1545416c166176584b900b6c6f2856
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-07-06 20:08:24 +0100

    Unit tests

--- test_index.py
+++ test_index.py
@@ -1,8 +1,19 @@
 import unittest
 
+from index import CommonLettersException, three_most_common_letters
+
 
 class TestIndex(unittest.TestCase):
-    def test_blah(self):
-        a = 'some'
-        b = 'some'
-        self.assertEqual(a, b)
+    def test_not_enough_distinct_letters(self):
+        source = 'aaaabbbbb'
+
+        with self.assertRaises(CommonLettersException) as context:
+            three_most_common_letters(source)
+
+    def test_happy_path(self):
+        source = 'abcabc aa b d'
+        expected = ['a', 'b', 'c']
+
+        actual = three_most_common_letters(source)
+
+        self.assertEquals(expected, actual)
